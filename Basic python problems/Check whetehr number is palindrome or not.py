c322ca743d5d8f3ac4a1aad35276b2a4449e7f57
monisha15112006 <monisha15112006_bcs28@mepcoeng.ac.in>
2026-04-13 11:33:16 +0530

    Create Check whetehr number is palindrome or not.py

---
+++ Basic python problems/Check whetehr number is palindrome or not.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number:"));
+b = str(a);
+reversed = b[::-1];
+result=print("The given number is a palindrome.")if b==reversed else print("The given number is not a palindrome.");
