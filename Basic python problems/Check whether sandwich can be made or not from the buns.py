253297428b92436eb54fddb6c05f8a5fff5604ee
monisha15112006 <monisha15112006_bcs28@mepcoeng.ac.in>
2026-04-13 11:16:51 +0530

    Create Check whether sandwich can be made or not from the buns.py

---
+++ Basic python problems/Check whether sandwich can be made or not from the buns.py
@@ -0,0 +1,7 @@
+t=int(input("Enter number of test cases:"));
+for i in range(1,t+1):
+   n=int(input("Enter number of buns:"))
+   if(n%4>1):
+      print("Yes,sandwich can be bought.")
+   else:
+      print("No sandwich cant be bought")
