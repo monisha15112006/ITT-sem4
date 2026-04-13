9ae33c82d2b93ff02baf1af813a73d72690b25df
monisha15112006 <monisha15112006_bcs28@mepcoeng.ac.in>
2026-04-13 11:36:59 +0530

    Create Money Denomination.py

---
+++ Basic python problems/Money Denomination.py
@@ -0,0 +1,10 @@
+amount = int(input("Enter your amount:"));
+notes_500 = amount // 500;
+amount %= 500;
+print("500:",notes_500);
+notes_200 = amount // 200;
+amount %= 200;
+print("200:",notes_200);
+notes_100 = amount // 100;
+amount %= 100;
+print("100:",notes_100);
